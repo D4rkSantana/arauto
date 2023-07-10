647d5653d7ad80199bc3ee3f694bbffcb8c6f224
unknown <esilva-s@student.42sp.org.br>
2023-07-09 23:04:53 -0300

    Working interface graphic

--- teste_iu.py
+++ teste_iu.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+def printAlgo():
+    print("ola mundo")
+
 janela = tk.Tk()
+
+title = tk.Label(janela, text="Titulo")
+buttom = tk.Button(janela, text="click aqui", command=printAlgo)
+title.pack()
+buttom.pack()
 
 janela.mainloop()
